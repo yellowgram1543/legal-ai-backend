faf68cc724ed242778a84863f60f87173b87c602
yellowgram1543 <anushaanand1543@gmail.com>
2025-09-15 12:05:25 +0530

    updated config with gemini key

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,12 @@
+# -------------------------
 import os
 from dotenv import load_dotenv
-
-# Load .env if present (for local dev)
 load_dotenv()
 
-PROJECT_ID = os.getenv("PROJECT_ID", "legal-doc-hackathon-2025")
+PROJECT_ID = os.getenv("PROJECT_ID")
 LOCATION = os.getenv("LOCATION", "us-central1")
 PROCESSOR_ID = os.getenv("PROCESSOR_ID", "fc048f040d3d92d0")
-MODEL_ID = os.getenv("MODEL_ID", "gemini-1.5-pro")
-GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+DOC_AI_KEY = os.getenv("DOC_AI_KEY")        # path to docai/gcs key json
+GEMINI_KEY = os.getenv("GEMINI_KEY")        # path to gemini key json
+MODEL_ID = os.getenv("MODEL_ID", "gemini-2.5-pro")
 RAW_BUCKET = os.getenv("RAW_BUCKET", "legal-ai-docs")
